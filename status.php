55e1f1ef114bc9b5ccf709d9c0d6af16d203b8ce
ifotn <rich@infrontofthenet.com>
2023-01-19 13:58:21 -0500

    l2/1 - status form

---
+++ status.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status</title>
+</head>
+<body>
+    <form action="save-status.php" method="get">
+        <fieldset>
+            <label for="status">Status:</label>
+            <input name="status" id="status" />
+        </fieldset>
+        <button>Save</button>
+    </form>
+</body>
+</html>
